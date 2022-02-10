fe9d5cc89cb6c58b36d32b536663e3e9efb7b77f
Backus <backus24@up.edu>
2022-02-09 16:25:26 -0800

    Changed Color of Cake

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -47,7 +47,7 @@ private CakeModel cakeModel = null;
         setWillNotDraw(false);
 
         //Setup our palette
-        cakePaint.setColor(0xFFC755B5);  //violet-red
+        cakePaint.setColor(Color.YELLOW);  //violet-red
         cakePaint.setStyle(Paint.Style.FILL);
         frostingPaint.setColor(0xFFFFFACD);  //pale yellow
         frostingPaint.setStyle(Paint.Style.FILL);
